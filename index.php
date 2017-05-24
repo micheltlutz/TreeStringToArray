ec52761230ab2341c445e906e5bf41b007ffb405
Michel Anderson Lutz Teixeira <michel@mastersmx.com>
2017-05-24 14:50:27 -0300

    Doc Licença

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * TreeStringToArray - Manipular Api retorno de containers Storage in Oracle Storage Cloud Service
+ * PHP Version ^5.6
+ * @link https://github.com/micheltlutz/TreeStringToArray TreeStringToArray GitHub project
+ * @author Michel Anderson Lütz Teixeira <michel@mastersmx.com>
+ * @copyright 2017 - Michel Anderson Lütz Teixeira
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
 require_once "Debug.class.php";
 require_once "TreeStringToArray.class.php";
 
