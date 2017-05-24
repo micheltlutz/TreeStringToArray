6205cd33a5600cee245b7ec058257a34dbd4bc0a
Michel Anderson Lutz Teixeira <michel@mastersmx.com>
2017-05-24 14:50:02 -0300

    Doc licença

--- Debug.class.php
+++ Debug.class.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * TreeStringToArray - Manipular Api retorno de containers Storage in Oracle Storage Cloud Service
+ * PHP Version ^5.x
+ * @link https://github.com/micheltlutz/TreeStringToArray TreeStringToArray GitHub project
+ * @author Michel Anderson Lütz Teixeira <michel@mastersmx.com>
+ * @copyright 2017 - Michel Anderson Lütz Teixeira
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
 class Debug
 {
   public static function exibe($_variavel, $_descricao = '', $_cor = '#FF0000', $_size = 2)
