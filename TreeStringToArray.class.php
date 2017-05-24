f8efdf9d35fe6474c21b50517350a64f83bdce2e
Michel Anderson Lutz Teixeira <michel@mastersmx.com>
2017-05-24 14:49:07 -0300

    Documentação de classe

--- TreeStringToArray.class.php
+++ TreeStringToArray.class.php
@@ -1,12 +1,42 @@
 <?php
+/**
+ * TreeStringToArray - Manipular Api retorno de containers Storage in Oracle Storage Cloud Service
+ * PHP Version ^5.6
+ * @link https://github.com/micheltlutz/TreeStringToArray TreeStringToArray GitHub project
+ * @author Michel Anderson Lütz Teixeira <michel@mastersmx.com>
+ * @copyright 2017 - Michel Anderson Lütz Teixeira
+ * @license http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
+ */
 class TreeStringToArray
 {
-  protected $array, $spliter;
+  /*
+  * Array para manipulação
+  * @var array
+  */
+  private $array;
+  /*
+  * String separadora de diretórios
+  * @var string
+  */
+  private $spliter;
+  
+  /**
+  * Construtor
+  * @param array $_array Array para manipulação
+  * @param string $_spliter String separadora de diretórios
+  * @access public
+  * @return array
+  */
   public function __construct($_array = array(), $_spliter = "/")
   {
     $this->array = $_array;
     $this->spliter = $_spliter;
   }
+  /**
+  * Processa array de diretórios em string retornando estrutura em array
+  * @access public
+  * @return array
+  */
   public function getArray()
   {
     $path_tree = array();
